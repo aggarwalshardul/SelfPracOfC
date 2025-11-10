bb8101d89cde2fb8f0fd13e3b5df2abd34028dc8
aggarwalshardul <aggarwalshardul@gmail.com>
2025-11-10 20:48:56 +0530

    Add files via upload

---
+++ asg01/while.c
@@ -0,0 +1,11 @@
+//WAP to print the first 100 natural numbers using while loop.
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    while (i <= 100) {
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
